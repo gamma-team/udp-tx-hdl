601f494de7284a45414d1195fc0dc02fb7dfa9db
MohammedIbra89 <des.m7md@gmail.com>
2017-04-01 18:56:05 -0400

    Update udp_tx_tb.vhd

--- sim/udp_tx_tb.vhd
+++ sim/udp_tx_tb.vhd
@@ -14,7 +14,6 @@ end udp_tx_tb;
 
 architecture Behavioral of udp_tx_tb is
 
-
 COMPONENT udp_tx
     GENERIC (
         width : POSITIVE := 8
@@ -39,8 +38,8 @@ PORT (
 );
 END COMPONENT;
 
-file In_file : text open read_mode is "input.txt";-- Change the file name
-file Out_file : text open read_mode is "output.txt";-- Change the file name
+file In_file : text open read_mode is "C:\tutorials\udp_tx\input.txt";-- Change the file name + Directory
+file Out_file : text open write_mode is "C:\tutorials\udp_tx\output.txt";-- Change the file name + Directory
 
 --Clock and Reset signals
 signal Clk: STD_LOGIC := '0';
@@ -60,12 +59,12 @@ signal Data_out_start : STD_LOGIC;
 signal Data_out_end : STD_LOGIC;
 signal Data_out_err : STD_LOGIC;
 
-
 signal TB_Completed: STD_LOGIC:= '0';
+signal Data_to_file: STD_LOGIC:= '0';
 
 begin
 
-DUT: udp_rx port map (
+DUT: udp_tx port map (
     Clk => Clk,
     Rst => Rst,
 	
@@ -83,46 +82,44 @@ DUT: udp_rx port map (
 );
 
 
--- Read file process
 process
+
     variable Buff_in: LINE;
     variable Data_input : STD_LOGIC_VECTOR(TB_width * 8 - 1 downto 0);
     variable Data_valid_input : STD_LOGIC_VECTOR(TB_width - 1 downto 0);
-begin
+    begin
     
     Data_in <= (others => '0');
     Data_in_valid <= (others => '0');
     Data_in_start <= '0';
     Data_in_end <= '0';
-    Data_err <= '0';
+    --Data_in_err <= '0';
+    
     -- wait for reset process to finish
     wait for 100 ns;
-	
+    wait until rising_edge(Clk);
     Data_in_start <= '1';
-    report "TB - Application data from file...";
-	
+    wait until rising_edge(Clk); 
+    
+    report "TB - Loadign Data messages from file...";
     while not endfile(In_file) loop
         readline(In_file, Buff_in);
         hread(Buff_in, Data_input); -- read first 8 bytes in file
         hread(Buff_in, Data_valid_input); -- read data_in_valid byte
-        
+        Data_in_start <= '0';
         Data_in_valid <= Data_valid_input;
         Data_in <= Data_input;
-
         wait until rising_edge(Clk);
     end loop;
-	
+    
     Data_in_end <= '1';
-	
     Data_in <= (others => '0');
     Data_in_valid <= (others => '0');
-    Data_in_start <= '0';
-    --Data_in_end <= '0';
-    --Data_err <= '0';
-	
     file_close(In_file);
-    report "TB - Application data have been loaded successfully";
+    report "TB - Data messages have been loaded successfully";
     TB_Completed <= '1';
+    wait until rising_edge(Clk);
+    Data_in_end <= '0';
     wait;
 end process;    
 
@@ -131,33 +128,31 @@ end process;
 process
     variable Buff_out : LINE;
     variable Data_out_actual : STD_LOGIC_VECTOR(TB_width * 8 - 1 downto 0);
-    variable Data_valid_out_actual : STD_LOGIC_VECTOR(TB_width * 8 - 1 downto 0);
-    variable Data_out_expected : STD_LOGIC_VECTOR(TB_width * 8 - 1 downto 0);
-    variable Data_valid_out_expected : STD_LOGIC_VECTOR(TB_width * 8 - 1 downto 0);
+    variable Data_valid_out_actual : STD_LOGIC_VECTOR(TB_width - 1 downto 0);
 begin
 
-    --Data_output := Data_out
     Data_out_actual := Data_out;
     Data_valid_out_actual := Data_out_valid;
 	
     if Data_out_start = '1' then
-        while not endfile(Out_file) loop
-            Readline(Out_file, Buff_out);
-            hread(Buff_out, Data_out_expected);
-            assert Data_out_actual = Data_out_expected 
-                report "Data in file does not match the output of module";
-            assert Data_valid_out_actual = Data_valid_out_expected 
-                report "Valid byte in file does not match the output valid bus from module";
-        end loop;
+        Data_to_file <= '1';
+    end if;
+    
+    if Data_to_file = '1' then
+        hwrite(Buff_out, Data_out);
+        hwrite(Buff_out, Data_out_valid);
+        writeline(Out_file, Buff_out);
     end if;
 	
     wait until rising_edge(Clk);
 	
-    if (TB_completed = '1') then
-        file_close(outputs);
+    if (Data_out_end = '1') then
+        Data_to_file <= '0';
+        file_close(Out_file);
         wait;
     end if;
 end process;
+
 
 -- clk process
 process
