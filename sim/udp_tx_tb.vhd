8ad47a8e40d061126a5126c3c96ee121979dd144
MohammedIbra89 <des.m7md@gmail.com>
2017-04-02 22:03:17 -0400

    Update udp_tx_tb.vhd

--- sim/udp_tx_tb.vhd
+++ sim/udp_tx_tb.vhd
@@ -38,8 +38,8 @@ PORT (
 );
 END COMPONENT;
 
-file In_file : text open read_mode is "C:\tutorials\udp_tx\input.txt";-- Change the file name + Directory
-file Out_file : text open write_mode is "C:\tutorials\udp_tx\output.txt";-- Change the file name + Directory
+file In_file : text open read_mode is "C:\tutorials\udp_tx\input.txt";-- Change the file name
+file Out_file : text open write_mode is "C:\tutorials\udp_tx\output.txt";-- Change the file name
 
 --Clock and Reset signals
 signal Clk: STD_LOGIC := '0';
@@ -87,70 +87,96 @@ process
     variable Buff_in: LINE;
     variable Data_input : STD_LOGIC_VECTOR(TB_width * 8 - 1 downto 0);
     variable Data_valid_input : STD_LOGIC_VECTOR(TB_width - 1 downto 0);
+    variable Data_start_input : STD_LOGIC;
+    variable Data_end_input : STD_LOGIC;
     begin
     
     Data_in <= (others => '0');
     Data_in_valid <= (others => '0');
     Data_in_start <= '0';
     Data_in_end <= '0';
-    --Data_in_err <= '0';
-    
+    Data_in_err <= '0';
     -- wait for reset process to finish
     wait for 100 ns;
-    wait until rising_edge(Clk);
-    Data_in_start <= '1';
-    wait until rising_edge(Clk); 
+    wait until rising_edge(clk);
     
-    report "TB - Loadign Data messages from file...";
+    report "TB - Loadign Application messages from file...";
     while not endfile(In_file) loop
         readline(In_file, Buff_in);
         hread(Buff_in, Data_input); -- read first 8 bytes in file
         hread(Buff_in, Data_valid_input); -- read data_in_valid byte
-        Data_in_start <= '0';
+        read(Buff_in,Data_start_input); -- read Data_in_start bit
+        read(Buff_in,Data_end_input); -- read Data_in_end bit
+        
+        Data_in_start <= Data_start_input;
+        Data_in_end <= Data_end_input;
         Data_in_valid <= Data_valid_input;
         Data_in <= Data_input;
+    
         wait until rising_edge(Clk);
     end loop;
     
-    Data_in_end <= '1';
+    Data_in_end <= '0';
     Data_in <= (others => '0');
     Data_in_valid <= (others => '0');
     file_close(In_file);
-    report "TB - Data messages have been loaded successfully";
+    report "TB - Application messages have been loaded successfully";
     TB_Completed <= '1';
-    wait until rising_edge(Clk);
-    Data_in_end <= '0';
     wait;
+
 end process;    
 
 
 -- Output process
 process
     variable Buff_out : LINE;
-    variable Data_out_actual : STD_LOGIC_VECTOR(TB_width * 8 - 1 downto 0);
-    variable Data_valid_out_actual : STD_LOGIC_VECTOR(TB_width - 1 downto 0);
 begin
-
-    Data_out_actual := Data_out;
-    Data_valid_out_actual := Data_out_valid;
-	
-    if Data_out_start = '1' then
+        if Data_out_start = '1' then
         Data_to_file <= '1';
     end if;
     
-    if Data_to_file = '1' then
-        hwrite(Buff_out, Data_out);
-        hwrite(Buff_out, Data_out_valid);
-        writeline(Out_file, Buff_out);
+    if Data_out_start = '1' or Data_to_file = '1' then
+        if Data_out_valid(0) = '1' then
+            hwrite(Buff_out, Data_out(7 downto 0));
+            write(Buff_out, STRING'(" "));
+        end if;
+        if Data_out_valid(1) = '1' then
+            hwrite(Buff_out, Data_out(15 downto 8));
+            write(Buff_out, STRING'(" "));
+        end if;
+        if Data_out_valid(2) = '1' then
+            hwrite(Buff_out, Data_out(23 downto 16));
+            write(Buff_out, STRING'(" "));
+        end if;
+        if Data_out_valid(3) = '1' then
+            hwrite(Buff_out, Data_out(31 downto 24));
+            write(Buff_out, STRING'(" "));
+        end if;
+        if Data_out_valid(4) = '1' then
+            hwrite(Buff_out, Data_out(39 downto 32));
+            write(Buff_out, STRING'(" "));
+        end if;
+        if Data_out_valid(5) = '1' then
+            hwrite(Buff_out, Data_out(47 downto 40));
+            write(Buff_out, STRING'(" "));
+        end if;
+        if Data_out_valid(6) = '1' then
+            hwrite(Buff_out, Data_out(55 downto 48));
+            write(Buff_out, STRING'(" "));
+        end if;
+        if Data_out_valid(7) = '1' then
+            hwrite(Buff_out, Data_out(63 downto 56));
+            write(Buff_out, STRING'(" "));
+        end if;
     end if;
-	
-    wait until rising_edge(Clk);
-	
+    
     if (Data_out_end = '1') then
+        writeline(Out_file, Buff_out);
         Data_to_file <= '0';
         file_close(Out_file);
         wait;
     end if;
+    wait until rising_edge(Clk);
 end process;
 
 
